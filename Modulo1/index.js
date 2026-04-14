354c1a1f936b84b4937cdd2ab31cc0b78c7ece34
Evanio Tech <evanioshark@gmail.com>
2026-04-14 07:54:02 -0300

    Módulo 1 finalizado

--- Modulo1/index.js
+++ Modulo1/index.js
@@ -46,3 +46,19 @@ let caixaFunctions = {
 console.log(caixaFunctions.vezes(5,5))
 console.log(caixaFunctions.dividir(5,5))
 caixaFunctions.aparecer()
+
+function multiply(a,b=1) {
+    return a * b
+}
+
+console.log(multiply(5))
+
+function bv() {
+    console.log('Bem vindo!!!!')
+}
+
+let obj = {
+    bv
+}
+
+obj.bv()
